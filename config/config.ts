ebd8d0c9063b39c62cbdbde9f9ff6b12fe1b7ad0
Dev25-os <bikeshgupta50041@gmail.com>
2024-04-17 16:34:08 +0530

    add express

---
+++ config/config.ts
@@ -0,0 +1,5 @@
+const _config = {
+  PORT: process.env.PORT,
+};
+
+export const config = Object.freeze(_config);
--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
-console.log("hello world!");
+import { config } from "./config/config";
+import app from "./src/app";
+
+let startServer = () => {
+  let PORT = config.PORT || 4000;
+  app.listen(PORT, () => {
+    console.log("Server listening on port:", PORT);
+  });
+};
+
+startServer();
