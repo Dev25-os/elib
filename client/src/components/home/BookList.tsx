b06303732dfa6559592a21b02fbddcebc1b01330
Dev25-os <bikeshgupta50041@gmail.com>
2024-06-14 17:25:14 +0530

    add try catch

--- client/src/components/home/BookList.tsx
+++ client/src/components/home/BookList.tsx
@@ -3,16 +3,21 @@ import React from "react";
 import BookCard from "./BookCard";
 
 const BookList = async () => {
-  // fetch book data
-  const response = await fetch(`${process.env.BACK_END}/books/getBooks`, {
-    cache: "no-store",
-  });
+  let books = null;
+  try {
+    // fetch book data
+    const response = await fetch(`${process.env.BACK_END}/books/getBooks`, {
+      cache: "no-store",
+    });
 
-  if (!response.ok) {
-    throw new Error("An Error occurred while fetching the books!");
+    if (!response.ok) {
+      throw new Error("An Error occurred while fetching the books!");
+    }
+
+    books = await response.json();
+  } catch (error) {
+    console.log("Error while fetching book details", error);
   }
-
-  const books = await response.json();
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3  gap-4">
