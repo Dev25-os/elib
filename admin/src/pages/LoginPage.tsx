7393b7ea50a0fc5c82c5026de80ba73c407b8462
Dev25-os <bikeshgupta50041@gmail.com>
2024-06-14 22:22:26 +0530

    add refs

--- admin/src/pages/LoginPage.tsx
+++ admin/src/pages/LoginPage.tsx
@@ -9,9 +9,20 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 export default function LoginPage() {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = () => {
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    console.log(email, password);
+  };
+
   return (
     <div className="flex h-screen w-full items-center justify-center">
       <Card className="w-full max-w-sm">
@@ -25,6 +36,7 @@ export default function LoginPage() {
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
             <Input
+              ref={emailRef}
               id="email"
               type="email"
               placeholder="m@example.com"
@@ -33,14 +45,16 @@ export default function LoginPage() {
           </div>
           <div className="grid gap-2">
             <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" required />
+            <Input ref={passwordRef} id="password" type="password" required />
           </div>
         </CardContent>
         <CardFooter className="flex flex-col">
-          <Button className="w-full">Sign in</Button>
+          <Button onClick={handleSubmit} className="w-full">
+            Sign in
+          </Button>
           <div className="mt-4 text-center text-sm">
             Don't have an account?{" "}
-            <Link to={"/register"} className="underline">
+            <Link to={"/auth/register"} className="underline">
               Sign up
             </Link>
           </div>
