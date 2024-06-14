d227942ca3033ee5e5ea5311e3e4aeec3ba50884
Dev25-os <bikeshgupta50041@gmail.com>
2024-06-14 21:53:52 +0530

    add auth layout

--- admin/src/router.tsx
+++ admin/src/router.tsx
@@ -5,6 +5,7 @@ import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import BooksPage from "./pages/BooksPage";
+import AuthLayout from "./layouts/AuthLayout";
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -20,12 +21,19 @@ export const router = createBrowserRouter([
       },
     ],
   },
+
   {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/register",
-    element: <RegisterPage />,
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "register",
+        element: <RegisterPage />,
+      },
+    ],
   },
 ]);
