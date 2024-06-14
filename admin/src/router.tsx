ca7006eb2bd26d10a9301cbbda32abd673dcf770
Dev25-os <bikeshgupta50041@gmail.com>
2024-06-14 21:33:00 +0530

    add dashboard components

--- admin/src/router.tsx
+++ admin/src/router.tsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Dashboard />,
   },
   {
     path: "/login",
