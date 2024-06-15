d858992aeedf8ee9aa7f7fedbf5d18ec5de497a3
Dev25-os <bikeshgupta50041@gmail.com>
2024-06-15 12:02:18 +0530

    implement protected route

--- admin/src/layouts/DashboardLayout.tsx
+++ admin/src/layouts/DashboardLayout.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Navigate, Outlet } from "react-router-dom";
 import {
   Bell,
   CircleUser,
@@ -31,8 +31,15 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { useTokenStore } from "@/store";
 
 const DashboardLayout = () => {
+  const token = useTokenStore((state) => state.token);
+
+  if (!token) {
+    return <Navigate to={"/auth/login"} replace />;
+  }
+
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
